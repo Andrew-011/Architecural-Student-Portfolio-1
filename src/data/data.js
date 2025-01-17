0b13e188ee261abe96c6fdfba026d8a0080fadac
Andrew Cortado <andrewcortado@andrews-mbp.ucalgary.ca>
2025-01-17 16:28:24 -0700

    Updated portfolio

--- src/data/data.js
+++ src/data/data.js
@@ -1,11 +1,11 @@
 // CUSTOMIZE THIS FILE TO ADD YOUR OWN DATA
 
 const data = {
-    name: "John Doe",
+    name: "Andrew Cortado",
     bio: "Undergraduate Architectural Student at University of Calgary passionate about building tools to make homes more affordable.",
     contact: {
-        email: "john.doe@gmail.com",
-        phone: "+12345678910",
+        email: "andrewcortado.ac@gmail.com",
+        phone: "+1 867 689 7676",
         linkedin: "https://www.linkedin.com/",
         github: "https://www.github.com/",
     }
